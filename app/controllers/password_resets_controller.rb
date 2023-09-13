1ab317178542ae6a7b0bc5ea7562e76728d4596b
Matt Pike <mattp91@gmail.com>
2023-09-13 10:59:34 -0400

    Password reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -9,12 +9,29 @@ class PasswordResetsController < ApplicationController
       # Send email
       # To setup mailer, enter this command: rails generate mailer Password reset
         # generate "mailer" generates a mailer, "Password" is name of mailer, and "reset" is an action to setup on the mailer controller
-      PasswordMailer.with(user: @user).reset.deliver_now
+      PasswordMailer.with(user: @user).reset.deliver_later
     end
     redirect_to root_path, notice: "If an account with that email was found, we have sent a link to reset your password."
   end
 
   def edit
-    @user = User.find_signed(params[:token], purpose: "password_reset")
+    @user = User.find_signed!(params[:token], purpose: "password_reset")
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    redirect_to sign_in_path, alert: "Your token has expired. Please try again."
+  end
+
+  def update
+    @user = User.find_signed!(params[:token], purpose: "password_reset")
+    if @user.update(password_params)
+      redirect_to sign_in_path, notice: "Your password was reset successfully. Please sign in."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation)
   end
 end
